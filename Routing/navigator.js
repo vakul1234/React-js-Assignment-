7df8e1048e406d77c2fd25de037eb1a2d8a24ad2
vakul1234 <venkateshramillast@gmail.com>
2020-06-08 21:38:45 +0530

    1 file modified

--- Routing/navigator.js
+++ Routing/navigator.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 const Navigator = () =>{
     return(
         <div>
-        <table className="jumbotron" style={{'font-family':'New Times Roman','width':'490px','height':'90px'}}>
+        <table className="jumbotron" style={{'fontFamily':'New Times Roman','width':'490px','height':'90px'}}>
           <tr>
-            <th colspan="3" style={{'font-family':'arial',color:'black'}}>I am Navigator Help you to Navigate</th>
+            <th colspan="3" style={{'fontFamily':'arial',color:'black'}}>I am Navigator Help you to Navigate</th>
           </tr>
           <tr>
-            <td><Link to="/" style={{'text-decoration':'none'}}>Home</Link></td>
-            <td><Link to="/aboutus" style={{'text-decoration':'none'}}>About Us</Link></td>
-            <td><Link to="/contactus" style={{'text-decoration':'none'}}>Contact Us</Link></td>
+            <td><Link to="/" style={{'textDecoration':'none'}}>Home</Link></td>
+            <td><Link to="/aboutus" style={{'textDecoration':'none'}}>About Us</Link></td>
+            <td><Link to="/contactus" style={{'textDecoration':'none'}}>Contact Us</Link></td>
           </tr>
         </table>
         </div>
